043921677d4e2146f45d0ce27d04e1dc2cda2d3e
nickeblewis <nickeblewis@gmail.com>
2014-01-10 14:44:27 +0000

    Got a lot working

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,40 +2,58 @@ angular.module('starter.controllers', [])
 
 .controller('AppCtrl', function($scope) {
   // Main app controller, empty for the example
+   $scope.$on('tab.shown', function() {
+    // Might do a load here
+     console.log("getting gps");
+    navigator.geolocation.getCurrentPosition(
+        function(position) {
+            place.lat = position.coords.latitude;
+            place.lng = position.coords.longitude;
+        },
+        function() {
+            alert('Error getting location');
+    });
+  });
+  $scope.$on('tab.hidden', function() {
+    console.log("Hidden");
+  });
 })
 
 // A simple controller that fetches a list of data
 .controller('ItemsTabCtrl', function($scope, $firebase, Items, geolocation) {
   // "Items" is a service returning mock data (services.js)
-  var URL= "https://farnborough.firebaseio.com"
-  // var ref = new Firebase("https://farnborough.firebaseio.com/places");
-  
-  $scope.items = $firebase(new Firebase(URL + '/places'));
-  //$scope.items = Items.all();
+    var URL= "https://farnborough.firebaseio.com"
+    // var ref = new Firebase("https://farnborough.firebaseio.com/places");
+    
+    $scope.items = $firebase(new Firebase(URL + '/places'));
+    //$scope.items = Items.all();
 
-  $scope.$on('tab.shown', function() {
-    // Might do a load here
-  });
-  $scope.$on('tab.hidden', function() {
-    // Might recycle content here
-  });
-var lat = 0, 
-        lng = 0;
-
-    geolocation.getCurrentPosition(
-        function(position) {
-            lat = position.coords.latitude;
-            lng = position.coords.longitude;
-        },
-        function() {
-            alert('Error getting location');
+  var lat = 0, 
+          lng = 0;
+    $scope.$on('tab.shown', function() {
+      // Might do a load here
+       console.log("getting gps");
+      navigator.geolocation.getCurrentPosition(
+          function(position) {
+              lat = position.coords.latitude;
+              lng = position.coords.longitude;
+          },
+          function() {
+              alert('Error getting location');
+      });
     });
-  $scope.place = {
-    "name": "",
-    "description": "",
-    "lat": lat,
-    "lng": lng
-  };
+    $scope.$on('tab.hidden', function() {
+      console.log("Hidden");
+    });
+
+
+    
+    $scope.place = {
+      "name": "",
+      "description": "",
+      "lat": lat,
+      "lng": lng
+    };
 
   
 
@@ -71,16 +89,7 @@ var lat = 0,
     });
   };
 
-  $scope.createPlace = function(place) {
-    
-   
-    $scope.items.$add({
-      name: place.name,
-      description: place.description,
-      lat: place.lat,
-      lng: place.lng
-    });
-  };
+ 
 
   $scope.showItem = function() {
     alert("Hello");
@@ -96,6 +105,60 @@ var lat = 0,
     $scope.items.$save();
   };
 
+  
+
+})
+
+.controller('NewPlaceCtrl', function($scope, $routeParams, $firebase, Items) {
+
+  var URL= "https://farnborough.firebaseio.com"
+  // var ref = new Firebase("https://farnborough.firebaseio.com/places");
+  
+  $scope.items = $firebase(new Firebase(URL + '/places'));
+  //$scope.items = Items.all();
+
+  var lat = 0, 
+      lng = 0;
+
+$scope.place = {
+    "name": "",
+    "description": "",
+    "lat": lat,
+    "lng": lng
+  };
+  $scope.$on('tab.shown', function() {
+    // Might do a load here
+    console.log("getting gps");
+    navigator.geolocation.getCurrentPosition(
+        function(position) {
+            $scope.place.lat = position.coords.latitude;
+            $scope.place.lng = position.coords.longitude;
+        },
+        function() {
+            alert('Error getting location');
+    });
+  });
+
+  $scope.$on('tab.hidden', function() {
+    console.log("Hidden");
+  });
+
+
+  
+  
+   $scope.createPlace = function(place) {
+    
+   
+    $scope.items.$add({
+      name: place.name,
+      description: place.description,
+      lat: place.lat,
+      lng: place.lng
+    });
+  };
+})
+
+.controller('MapCtrl', function($scope, $routeParams, $firebase, Items) {
   angular.extend($scope, {
     farnborough: {
       lat: 51.293,
@@ -129,7 +192,6 @@ var lat = 0,
       scrollWheelZoom: false
     }
   });
-
 })
 
 // A simple controller that shows a tapped item's data
